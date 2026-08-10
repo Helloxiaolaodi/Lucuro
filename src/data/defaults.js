9500d2641ac757b4d06eaa6f57c1480bdbd63944
Yangsanduo <yanglun2019@126.com>
2026-08-10 19:09:12 +0800

    feat: default to browser bookmarks when no data source is saved

--- src/data/defaults.js
+++ src/data/defaults.js
@@ -29,7 +29,7 @@ export const DEFAULT_HITOKOTO = [
 
 export const DEFAULT_SETTINGS = {
   theme: 'light',
-  dataSource: null,
+  dataSource: 'browser',
   cardSize: 'default',
   cardRadius: 14,
   cardFontSize: 15,
